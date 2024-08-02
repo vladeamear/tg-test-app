95311a023cef95c238aa352c816fad167b6e6859
vladeamear <looser.s.usami@gmail.com>
2024-08-02 10:28:21 +0300

    add close button

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,15 @@ function App() {
   const [count, setCount] = useState(0);
   const inc = () => setCount((prev) => prev + 1);
 
+  const closeWebApp = () => window.Telegram.WebApp.close();
+
   useLayoutEffect(() => {
     window.Telegram.WebApp.MainButton.onClick(inc);
     window.Telegram.WebApp.MainButton.setText("+1");
     window.Telegram.WebApp.MainButton.show();
+
+    window.Telegram.WebApp.BackButton.onClick(closeWebApp);
+    window.Telegram.WebApp.BackButton.show();
   });
 
   return (
@@ -22,6 +27,9 @@ function App() {
       <h1>Hello World</h1>
       <h3>{count}</h3>
       <button onClick={inc}>+1</button>
+      <button onClick={closeWebApp} className="outline">
+        close
+      </button>
     </>
   );
 }
